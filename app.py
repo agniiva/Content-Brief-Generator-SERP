a68e434132f3dc5e2823a5164046b89e63ae6f5e
Agniva Mahata <73607864+agniiva@users.noreply.github.com>
2023-10-31 13:14:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import requests
 from bs4 import BeautifulSoup
+import openai
 
 def get_top_sites(keyword, apiKey):
     url = "https://google.serper.dev/search"
@@ -23,18 +24,41 @@ def scrape_site(url, tags):
         data[tag] = [elem.get_text(strip=True) for elem in soup.find_all(tag)]
     return data
 
+def generate_hyper_optimized_brief(consolidated_data, openai_api_key):
+    # Introductory context
+    # detailed_message = "You are a seasoned SEO expert and content strategist. Your task is to analyze the following consolidated data from top-ranking websites and provide a hyper-optimized content brief and thesis. This brief should be actionable and clear for content writers, highlighting key points and takeaways.\n\n"
+
+    # Add the scraped data
+    for tag, texts in consolidated_data.items():
+        detailed_message = f"{tag}: {' '.join(texts[:5])}\n"  # Using first 3 items for brevity
+
+    # Additional instructions for clarity and conciseness
+    detailed_message += "\nConsider the most relevant information, avoid fluff, and provide a concise yet comprehensive brief about the intent & content using the top ranking sites. Generate a optimised content brief with investment thesis."
+
+    openai.api_key = openai_api_key
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo-16k",
+        messages=[
+            {"role": "system", "content": "You are a seasoned SEO expert and content strategist. Your task is to analyze the following consolidated data from top-ranking websites and provide a hyper-optimized content brief and thesis. This brief should be actionable and clear for content writers, highlighting key points and takeaways.\n\n"},
+            {"role": "user", "content": detailed_message}
+        ]
+    )
+
+    return completion.choices[0].message["content"]
+
 st.title('SEO Tag Scraper')
 st.write("""
 This tool allows you to scrape SEO-related tags from the top-ranking sites for a specific keyword.
-To get started, you'll need an API key from SERPer Dev. If you don't have one, get it [here](https://serper.dev).
+To get started, you'll need an API key from SERPer Dev & OpenAI Api (it uses gpt-3.5-turbo-16k) If you don't have one, get it [SERPer Dev](https://serper.dev) & [OpenAI](https://platform.openai.com/).
 """)
 
 # Sidebar for API Key input
 st.sidebar.header("Settings")
 st.sidebar.write("""
-Please enter your SERPer Dev API key below. This key is used to fetch the top-ranking sites for your keyword.
+Please enter your SERPer Dev & OpenAI API key below. This key is used to fetch the top-ranking sites for your keyword, & using it give you a hyper personalised content brief
 """)
-apiKey = st.sidebar.text_input("Enter your SERP API Key:", type="password")
+serp_api_key = st.sidebar.text_input("Enter your SERP API Key:", type="password")
+openai_api_key = st.sidebar.text_input("Enter your OpenAI API Key:", type="password")
 
 # UI Components using columns
 col1, col2 = st.columns(2)
@@ -44,26 +68,38 @@ with col2:
     selected_tags = st.multiselect('Select tags to scrape:', ['h1', 'h2', 'h3', 'p'])
 
 if st.button('Scrape'):
-    # Check if API key is provided
-    if not apiKey:
+    # Check if SERP API key is provided
+    if not serp_api_key:
         st.warning("Please provide the SERP API Key in the sidebar.")
-        st.stop()  # Use st.stop() to halt the app execution at this point
+        st.stop()
 
     # Show a spinner with the message "Processing..."
     with st.spinner('Processing...'):
         # Call the SERP API
-        top_sites = get_top_sites(keyword, apiKey)
+        top_sites = get_top_sites(keyword, serp_api_key)
         
         # Scrape the Websites
         results = {}
         for site in top_sites:
             results[site] = scrape_site(site, selected_tags)
-    
+
     # Display the Results in Streamlit using expanders
+    consolidated_data = {}
     for site, data in results.items():
-        with st.expander(site):
+        with st.expander(f"Scraped Data from {site}"):
             for tag, texts in data.items():
                 st.markdown(f"**{tag}**")
                 for text in texts:
                     st.write(text)
-                st.write("---")
+                # Consolidate data for hyper-optimized brief
+                if tag not in consolidated_data:
+                    consolidated_data[tag] = []
+                consolidated_data[tag].extend(texts)
+
+    # Generate and display hyper-optimized content brief and thesis, only if OpenAI API key is provided
+    if openai_api_key:
+        with st.expander("Hyper-Optimized Content Brief & Thesis"):
+            hyper_brief = generate_hyper_optimized_brief(consolidated_data, openai_api_key)
+            st.write(hyper_brief)
+    else:
+        st.write("OpenAI API key not provided. Skipping hyper-optimized content brief generation.")
